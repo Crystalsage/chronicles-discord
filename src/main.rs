bf3edebcac702315661897ebef82c10492ce3ab8
Amit <amitkulkarni2028@gmail.com>
2022-10-02 00:17:52 +0530

    Integrate with chronicles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 
 use serenity::async_trait;
@@ -8,18 +9,30 @@ use serenity::Client;
 
 struct Handler;
 
+
+// We'll consider the 
+// - Timestamp ==> [Date, Time]
+// - Author
+// - Message
+
 #[async_trait]
 impl EventHandler for Handler {
     // For when the bot receives a message
     async fn message(&self, ctx: Context, msg: Message) {
+        let mut latest_message = HashMap::new();
+
+        latest_message.insert("timestamp", msg.timestamp.unix_timestamp().to_string());
+        latest_message.insert("content", msg.content);
+        latest_message.insert("author", "Bourbon".to_string());
+
         println!("I got a message. Replying to it");
 
-        let reply = "Your message was: ".to_owned() + &msg.content;
+        let request = reqwest::Client::new();
 
         if msg.author.name == "Bourbon" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, reply).await {
-                println!("Error sending message: {:?}", why);
-            }
+            request.post("http://127.0.0.1:8080")
+                .json(&latest_message)
+                .send().await.unwrap();
         }
     }
 
